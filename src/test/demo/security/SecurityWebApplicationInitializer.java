838841d6a8e41d8313e68912baba60809e17d78f
Premnath Christopher <premnath.christopher@urssystems.com>
2017-06-06 17:32:59 +0530

    Basic Authentication Done

---
+++ src/test/demo/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package test.demo.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
